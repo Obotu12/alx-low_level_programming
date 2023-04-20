4c45b59812c627f72a41c1de2bdfe80acb2e029d
Obotu12 <idokodavidobotuswentex@gmail.com>
2023-04-19 21:53:30 -0700

    print Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name:name of the person
+ * @f: pointer points to function f
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
